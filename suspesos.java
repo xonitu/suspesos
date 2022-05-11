55bd5e1a463c168be36492a240545ffba290b842
xonitu <104739531+xonitu@users.noreply.github.com>
2022-05-11 15:13:57 +0200

    Update suspesos.java

--- suspesos.java
+++ suspesos.java
@@ -1,15 +1,17 @@
 public class MyClass {
     public static void main(String[] args) {
+        
+        int count = 0;
+        
     float[] arrayNotes = {2f, 5.5f, 9f, 10f, 4.9f, 8f, 8.5f, 7f}; 
     for(int i = 0; i < arrayNotes.length; i++) {
     System.out.println(i+". és: " + arrayNotes[i]); }
-    //Número  de suspesos
-        int count = 0;
+    //calcul
         for (int j = 0; j < arrayNotes.length; j++) {
             if (arrayNotes[j] < 5) {
                 count++;
             }
         }
-        System.out.println("Hi ha" + count + "suspesos.");
+        System.out.println("Hi ha " + count + " suspesos.");
     }
 }
